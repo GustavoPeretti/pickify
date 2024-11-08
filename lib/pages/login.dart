a74a43f68131dd9984f6904830e32be0cd4c8d69
dgthaina <thainadagostini61@gmail.com>
2024-11-08 10:12:41 -0300

    scaffold padrao

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pickify/widgets/scaffold.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ScaffoldPadrao(
       body: Column(
         children: [
           Text('PICKIFY', style: TextStyle(color: Color.fromARGB(30, 185, 85, 1),))
