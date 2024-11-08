f4a677d2a503c556100d62654d9d65d906dfabeb
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-08 11:21:31 -0300

    Autorização

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pickify/pages/login.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MainApp());
+  
 }
 
 class MainApp extends StatelessWidget {
