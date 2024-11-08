1d157dc1591f55d3064018239f6e3898c6062930
GustavoPeretti <gustavoperetti11@gmail.com>
2024-11-08 09:19:33 -0300

    Adição de fontes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
+      ),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        textTheme: GoogleFonts.rudaTextTheme(),
+        scaffoldBackgroundColor: const Color.fromRGBO(8, 8, 8, 1)
       ),
     );
   }
